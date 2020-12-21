0c5b6f24ad54a1416f741711927478f4c6f1f92e
Piotr Dopierala <piotr.dopierala@contractors.roche.com>
2020-12-21 15:26:52 +0100

    adds alias entpoint to top 5 cheap tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAvaredge,price';
+  req.query.fields = 'name,price,ratingsAvaredge,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD QUERY
